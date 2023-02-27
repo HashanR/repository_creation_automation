25a5db40c0f314c5110ba49b864ac298d46e515a
HashanR <rmhashan@gmail.com>
2023-02-28 01:40:54 +0530

    add workflow

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 import os
+import sys
 
-# GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_TOKEN")
-GITHUB_ACCESS_TOKEN = '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'
+GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_TOKEN")
+# GITHUB_ACCESS_TOKEN = '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'
 CONFIG_FILE_NAME = 'repository_details.json'
 LOG_CONFIG_FILE_NAME = 'logging.json'
 WORKFLOW_CONFIG_FILE_NAME = 'workflow_config.json'
@@ -9,3 +10,5 @@ README_CONTENT = "# My Project\n\nThis is my project."
 ORG = 'HashanR'
 DIR_TO_COPY = '.github/workflows'
 WORKFLOW_COPY_FROM_REPOSITORY = 'HashanR/test_workflows'
+REPOSITORY_NAME = sys.argv[1]
+TEAM_NAME = sys.argv[2]
