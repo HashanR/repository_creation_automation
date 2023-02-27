54997a59318f62c2420cdd2179195170ee059ad1
HashanR <rmhashan@gmail.com>
2023-02-28 01:59:50 +0530

    change workflow

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_TOKEN")
-# GITHUB_ACCESS_TOKEN = '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'
+# GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_TOKEN")
+GITHUB_ACCESS_TOKEN = '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'
 CONFIG_FILE_NAME = 'repository_details.json'
 LOG_CONFIG_FILE_NAME = 'logging.json'
 WORKFLOW_CONFIG_FILE_NAME = 'workflow_config.json'
